410eb65a7d0346eb2e87f4ba6080833828167c6e
syhirrh <syasyahirah31@gmail.com>
2022-05-26 16:38:39 +0800

    second commit

--- next_page.php
+++ next_page.php
@@ -3,6 +3,11 @@
     session_start();
     echo "<html>";
 
+    $sid = session_id();
+    echo "Session ID returned by session_id(): ".$sid."\n";
+    $sid = SID;
+    echo "Session ID returned by SID: ".$sid."\n";
+
     $myLogin = $_SESSION["myLogin"];
     echo "Value of myLogin has been retrived: ".$myLogin."\n";
 
